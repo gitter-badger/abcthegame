8b4e67979e378e8d54c990ae252532737e491df3
Gnesis González <gnesis.gonzalez@gmail.com>
2014-06-13 15:51:22 -0500

    Proyecto Base
    ABC The Game - Game Jam - Mermelada de juegos 3: Autopoiesis

---
+++ abc/config.lua
@@ -0,0 +1,19 @@
+-- config.lua for project: ABC
+
+application =
+{
+	content =
+	{
+		-- width = 640,
+		-- height = 1136,
+		scale = "zoomEven", -- zoom to fill screen, possibly cropping edges
+		fps = 60,
+		antialias = true,
+                
+		imageSuffix =
+		{
+			["@2x"] = 2,
+		},
+	},
+}
+
